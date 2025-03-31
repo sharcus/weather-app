5d4655c4c9073d0c8efa2eb20d4cb462a947c051
Mikalai Kalpinski <kalpinski@gmail.com>
2025-03-31 15:25:14 +0300

    Update color scheme

--- src/pages/Month.jsx
+++ src/pages/Month.jsx
@@ -73,7 +73,7 @@ export default function Month() {
             <div className="chart-container">
               <ResponsiveContainer width="100%" height={500}>
                 <LineChart data={data}>
-                  <Line type="monotone" dataKey="tmp" stroke="#8884d8" />
+                  <Line type="monotone" dataKey="tmp" stroke="dodgerblue" />
                   <Tooltip content={<CustomTooltip />} />
                   <CartesianGrid />
                   <XAxis interval={7} tickFormatter={(tick) => tick > 0 ? tick / 8 + 1 : 1}  /> 
